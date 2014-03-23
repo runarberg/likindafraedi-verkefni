4ea589092f2ad931ad93b40f690f92789f104480
Rúnar Berg Baugsson Sigríðarson <runarberg@zoho.com>
2014-03-23 14:33:24 +0000

    glærur tilbúnar

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 all:
 	R -e "library('knitr'); knit('lausnir.Rmd', quiet=TRUE)" --slave
 	pandoc -f markdown -t html5 \
+	--section-divs \
+	--css="buttondown.css" -s \
+	--mathjax="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML" \
+	--highlight-style pygments < lausnir.md > index.html
+	pandoc -f markdown -t html5 \
 	--section-divs -A scripts.html \
 	--css="buttondown.css" -s \
 	--mathjax="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML" \
--- script.js
+++ script.js
@@ -1,3 +1,58 @@
-$.ready(function() {
-    
+$("body > header").addClass("column");
+$("section > section").addClass("column");
+$("body > section").each(function () {
+    $(this).find("> h1, > p").wrapAll("<div class='column' />");
+});
+
+var $cols = $(".column");
+var numCols = $cols.length;
+var winWidth = $(window).width();
+var winHeight = $(window).height();
+
+$("body").css({
+    display: "-webkit-flex",
+    display: "flex",
+    width: (numCols) * winWidth
+});
+$("body > section").css({
+    display: "-webkit-flex",
+    display: "flex",
+    width: function () {
+        return $(this).find(".column").length * winWidth;
+    }
+});
+$cols.css({
+    width: winWidth*.85,
+    padding: winWidth*.075,
+    'min-height': winHeight,
+    'vertical-align': "top"
+});
+
+$(document).ready(function() {
+    var slide = 0;
+    $("html").keydown(function(e) {
+        switch(e.which) {
+            case 39:
+            if (slide < numCols) {
+                $("html, body").animate({
+                    scrollLeft: $($cols[slide+1]).offset().left,
+                    scrollTop: 0
+                }, 800);
+                slide++;
+                return false;
+            }
+            break;
+            
+            case 37:
+            if (slide > 0) {
+                $("html, body").animate({
+                    scrollLeft: $($cols[slide-1]).offset().left,
+                    scrollTop: 0
+                }, 800);
+                slide--;
+                return false;
+            }
+            break;
+        }
+    });
 });
